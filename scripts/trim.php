6ba065ffe3c2ebbed1a61ebdff87b636388ce581
Niklas K <picnik2@online.de>
2011-08-23 00:24:41 +0200

    added trim.php

---
+++ scripts/trim.php
@@ -0,0 +1,23 @@
+<?php
+ob_start();
+
+// --- needs to be modified ---
+$source = "/path/to/ladderlog.txt";
+$target = "/path/to/trimmed/log.txt";
+
+// --- don't change the rest --- 
+$needle = array("PLAYER_ENTERED", "PLAYER_RENAMED");
+
+
+foreach(file($source) as $haystack)
+{
+      if((strpos($haystack,$needle[0]) !== false) || (strpos($haystack,$needle[1]) !== false))
+            echo trim(str_replace($needle,"",$haystack));
+}
+
+
+$output = ob_get_contents(); 
+file_put_contents($target,$output);
+
+ob_end_clean();
+?>
